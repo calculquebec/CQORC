d1b76b0529558082a150c7452f6948a43aa62c9d
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2024-01-24 17:55:03 -0500

    add_panelist() and get_panelists()

--- interfaces/zoom/ZoomInterface.py
+++ interfaces/zoom/ZoomInterface.py
@@ -4,9 +4,15 @@ import requests
 from datetime import datetime
 
 class ZoomInterface:
-    # constants
+    '''Constants
+
+    References:
+        https://developers.zoom.us/docs/integrations/oauth/
+        https://developers.zoom.us/docs/api/rest/reference/zoom-api/methods/
+    '''
     auth_token_url = "https://zoom.us/oauth/token"
     api_base_url = "https://api.zoom.us/v2"
+
 
     def __init__(self, account_id, client_id, client_secret, timezone = "America/Montreal", user = "me"):
         '''Initialize the Zoom interface object with API credentials
@@ -14,12 +20,12 @@ class ZoomInterface:
         Reference: https://developers.zoom.us/docs/zoom-rooms/s2s-oauth/
 
         Arguments:
-            account_id -- String. Usually stored secretly
-            client_id -- String. Usually stored secretly
-            client_secret -- String. Usually stored secretly
-            timezone -- String. Valid values are in all_timezones from pytz
+            account_id -- string. Usually stored secretly
+            client_id -- string. Usually stored secretly
+            client_secret -- string. Usually stored secretly
+            timezone -- string. Valid values are in all_timezones from pytz
                 https://pythonhosted.org/pytz/#helpers
-            user -- String. Zoom username, either email address or "me"
+            user -- string. Zoom username, either email address or "me"
         '''
 
         self.account_id = account_id
@@ -39,19 +45,20 @@ class ZoomInterface:
         Returns: dictionary with keys "Authorization" and "Content-Type"
         '''
 
-        data = {
-            "grant_type": "account_credentials",
-            "account_id": self.account_id,
-            "client_secret": self.client_secret
-        }
-        response = requests.post(self.auth_token_url,
-                                 auth=(self.client_id, self.client_secret),
-                                 data=data)
-        assert response.status_code == 200, "Unable to get access token"
+        response = requests.post(
+            self.auth_token_url,
+            auth=(self.client_id, self.client_secret),
+            data={
+                "grant_type": "account_credentials",
+                "account_id": self.account_id,
+                "client_secret": self.client_secret
+            })
 
         response_data = response.json()
-        access_token = response_data["access_token"]
 
+        assert response.status_code == 200, response_data['message']
+
+        access_token = response_data["access_token"]
         headers = {
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/json"
@@ -111,10 +118,74 @@ class ZoomInterface:
         # https://developers.zoom.us/docs/api/rest/reference/zoom-api/methods/#operation/webinarCreate
         pass
 
-    def add_panelist(self):
-        # to be done
-        # https://developers.zoom.us/docs/api/rest/reference/zoom-api/methods/#operation/webinarPanelistCreate
-        pass
+
+    def add_panelist(self, webinar_id, email:str, name:str):
+        '''Add one panelist to the specified webinar by ID
+
+        Reference:
+            https://developers.zoom.us/docs/api/rest/reference/zoom-api/methods/#operation/webinarPanelistCreate
+
+        Arguments:
+            webinar_id -- int or string. To specify the webinar by ID.
+            email -- string. Email address of the panelist
+            name -- string. Full name of the panelist
+        '''
+
+        response = requests.post(
+            f'{self.api_base_url}/webinars/{webinar_id}/panelists',
+            json={'panelists': [{
+                'email': email,
+                'name': name
+            }]},
+            headers=self.get_authorization_header(),
+        )
+
+        assert response.status_code == 201, response.json()['message']
+
+
+    def get_panelists(self, webinar_id):
+        '''Get a list of all panelists, one dictionary per panelist
+
+        Reference:
+            https://developers.zoom.us/docs/api/rest/reference/zoom-api/methods/#operation/webinarPanelists
+
+        Arguments:
+            webinar_id -- int or string. To specify the webinar by ID.
+
+        Returns: a list of dictionaries with fields 'name' and 'email'
+        '''
+
+        response = requests.get(
+            f'{self.api_base_url}/webinars/{webinar_id}/panelists',
+            headers=self.get_authorization_header(),
+        )
+        response_data = response.json()
+
+        assert response.status_code == 200, response_data['message']
+
+        return response_data['panelists']
+
+
+    def get_webinar(self, webinar_id):
+        '''Get a single scheduled webinar by the ID
+
+        Reference:
+            https://developers.zoom.us/docs/api/rest/reference/zoom-api/methods/#operation/webinar
+
+        Arguments:
+            webinar_id -- int or string. To select the webinar by ID.
+
+        Returns: a dictionary with the information of one webinar
+        '''
+
+        response = requests.get(
+            f'{self.api_base_url}/webinars/{webinar_id}',
+            headers=self.get_authorization_header())
+        response_data = response.json()
+
+        assert response.status_code == 200, response_data['message']
+
+        return response_data
 
 
     def get_webinars(self, date = None, ids = None):
@@ -127,49 +198,32 @@ class ZoomInterface:
             date -- datetime.datetime object, or None. Select by date.
             ids -- list or set of webinar ID codes, or None. Select by ids.
 
-        Returns: list of webinars, one dictionary per webinar
+        Returns: list of webinars, one dictionary per webinar, or []
         '''
 
         url = f'{self.api_base_url}/users/{self.user}/webinars'
+        payload = {'type': 'scheduled'}
         headers = self.get_authorization_header()
-        params = {
-            "type": "scheduled",
-            "page_size": "300",
-        }
 
-        response = requests.get(url, params=params, headers=headers).json()
-        all_webinars = response.get('webinars', None)
-        next_page_token = response.get('next_page_token', None)
+        all_webinars = []
+        next_page_token = 'The first query is done without a real token'
 
-        # Get next pages if there is any
         while next_page_token:
-            params['next_page_token'] = next_page_token
-            response = requests.get(url, params=params, headers=headers).json()
-            all_webinars += response.get('webinars', [])
-            next_page_token = response.get('next_page_token', None)
+            response = requests.get(url, params=payload, headers=headers)
+            response_data = response.json()
+            assert response.status_code == 200, response_data['message']
+
+            all_webinars += response_data.get('webinars', [])
+            next_page_token = response_data.get('next_page_token', None)
+            payload['next_page_token'] = next_page_token
 
         webinars = all_webinars
         if date:
             webinars = [w for w in all_webinars if datetime.fromisoformat(w['start_time']).date() == date]
         if ids:
             webinars = [w for w in all_webinars if w['id'] in ids]
+
         return webinars
-
-
-    def get_webinar(self, webinar_id):
-        # https://developers.zoom.us/docs/api/rest/reference/zoom-api/methods/#operation/webinars
-        headers = self.get_authorization_header()
-        payload = {
-            "type": "scheduled",
-            "page_size": "300",
-        }
-
-        resp = requests.get(f"{self.api_base_url}/webinars/{webinar_id}",
-                             headers=headers,
-                             params=payload)
-
-        response = resp.json()
-        return response
 
 
     def update_webinar(self):
@@ -208,6 +262,7 @@ def main():
     '''
 
     from common import get_config
+    import pytz
 
     class DefaultArgs:
         '''Some attributes with default values for get_config()'''
@@ -217,6 +272,7 @@ def main():
     # Get all configured values
     config = get_config(DefaultArgs())
     zoom_cfg = config['zoom']
+    tzinfo = pytz.timezone(config['global']['timezone'])
 
     # Create the Zoom interface object
     zoom = ZoomInterface(
@@ -226,10 +282,50 @@ def main():
         timezone = config['global']['timezone'],
         user = zoom_cfg['user'])
 
-    #print(str(zoom.create_meeting("Meeting test", "60", "2023-11-10", "13:00:00")))
-    for webinar in zoom.get_webinars():
-        print(webinar['id'], webinar['start_time'][:16],
-              webinar['topic'][-48:])
+    # Create one meeting
+    '''
+    print(str(zoom.create_meeting("Meeting test", "60", "2023-11-10", "13:00:00")))
+    '''
+
+    # Overview of all webinars : ID Local-Start-Time End-of-topic
+    webinars = zoom.get_webinars()
+    print(f'{len(webinars)} webinar(s):')
+    print(f"{'Webinar-ID':11} {'Date':10} {'Time':5}",
+          'Session topic (last 48 characters)')
+
+    for w in webinars:
+        start_time = datetime.fromisoformat(w['start_time']).astimezone(tzinfo)
+        print(w['id'], str(start_time)[:-9], w['topic'][-48:])
+
+    # Detailed view of a single webinar
+    webinar_id = 82199833482
+    webinar = zoom.get_webinar(webinar_id)
+
+    '''
+    for attribute, value in webinar.items():
+        if attribute == 'settings':
+            print(f'{attribute}:')
+            for setting_name, setting_value in value.items():
+                print(f'- {setting_name}: {setting_value}')
+        else:
+            print(f'{attribute}: {value}')
+    '''
+
+    # Add a panelist to a signle webinar
+    '''
+    zoom.add_panelist(
+        webinar_id,
+        'first_name.last_name@calculquebec.ca',
+        'Full Name')
+    '''
+
+    # Print all panelists of a single webinar
+    '''
+    start_t = datetime.fromisoformat(webinar['start_time']).astimezone(tzinfo)
+    print(f"Panelists of {str(start_t)[:-9]} - \"{webinar['topic']}\":")
+    for p in zoom.get_panelists(webinar_id):
+        print(f"- \"{p['name']}\" <{p['email']}>")
+    '''
 
 
 if __name__ == "__main__":
