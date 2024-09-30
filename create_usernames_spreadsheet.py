c229c3508507e3a5c017421d5ac1e5ca042e3364
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-09-30 09:08:06 -0400

    bug fixes and support course_id

--- create_usernames_spreadsheet.py
+++ create_usernames_spreadsheet.py
@@ -5,11 +5,14 @@ import os, argparse, datetime
 import interfaces.eventbrite.EventbriteInterface as Eventbrite
 import interfaces.google.GDriveInterface as GDriveInterface
 import interfaces.google.GSheetsInterface as GSheetsInterface
+import interfaces.slack.SlackInterface as SlackInterface
+import CQORCcalendar
 
 from common import valid_date, to_iso8061, ISO_8061_FORMAT, get_config
 from common import extract_course_code_from_title
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--course_id", default=None, help="Manage only for this course id")
 parser.add_argument("--event_id", help="EventBrite event id")
 parser.add_argument("--next", default=False, action='store_true', help="Generate for the next event after now")
 parser.add_argument("--date", metavar=ISO_8061_FORMAT, type=valid_date, help="Generate for the first event on this date")
@@ -32,6 +35,14 @@ credentials_file_path = os.path.join(secrets_dir, credentials_file)
 # initialize the Google Drive interface
 gdrive = GDriveInterface.GDriveInterface(credentials_file_path)
 gsheets = GSheetsInterface.GSheetsInterface(credentials_file_path)
+calendar = CQORCcalendar.Calendar(global_config, args)
+
+if args.course_id:
+    if args.course_id in calendar.keys():
+        course = calendar[args.course_id]
+    else:
+        print(f"Course {args.course_id} not found")
+        exit(1)
 
 # this script's config
 config = global_config['script.usernames']
@@ -42,9 +53,16 @@ if args.create_template_file:
 
 # initialize EventBrite interface:
 eb = Eventbrite.EventbriteInterface(global_config['eventbrite']['api_key'])
+
+eventbrite_id =None
+if course:
+    eventbrite_id = course['sessions'][0]['eventbrite_id']
+elif args.event_id:
+    eventbrite_id = args.event_id
+
 # retrieve event from EventBrite
-if args.event_id:
-    event = eb.get_event(args.event_id)
+if eventbrite_id:
+    event = eb.get_event(eventbrite_id)
 else:
     events = eb.get_events(global_config['eventbrite']['organization_id'], time_filter="current_future", flattened=True, order_by="start_asc")
     for e in events:
@@ -54,9 +72,15 @@ else:
         elif args.date and  to_iso8061(e['start']['local']).date() == to_iso8061(args.date).date():
             event = e
             break
+    eventbrite_id = event['id']
+
+# course was not specified, find it from eventbrite_id
+if not course:
+    courses = [course for course in calendar.get_courses() if course['sessions'][0]['eventbrite_id'] == eventbrite_id]
+    course = courses[0]
 
 # retrieve list of attendees
-attendees = eb.get_event_attendees_registered(event['id'], fields = ['email', 'name'])
+attendees = eb.get_event_attendees_registered(eventbrite_id, fields = ['email', 'name'])
 
 date = to_iso8061(event["start"]["local"]).date()
 title = event["name"]["text"]
@@ -65,7 +89,7 @@ locale = event["locale"].split('_')[0]
 if args.course_code:
     course_code = args.course_code
 else:
-    course_code = extract_course_code_from_title(global_config, title)
+    course_code = course['sessions'][0]['code']
 
 if args.url:
     url = args.url
@@ -105,23 +129,20 @@ gsheets.update_values(sheet_id, data_range, data)
 # clone the spreadsheet protection
 gsheets.copy_protection(source_file_id, sheet_id)
 
-sheet_url = gdrive.get_file_id(sheet_id)
+sheet_url = gdrive.get_file_url(sheet_id)
 
 print(f"URL: {sheet_url}")
 
 # post to Slack
 slack = SlackInterface.SlackInterface(global_config['slack']['bot_token'])
 
-start = to_iso8061(eb_event['start']['local'])
-date = start.date()
-locale = eb_event['locale'].split('_')[0]
-course_code = extract_course_code_from_title(global_config, eb_event["name"]["text"])
-channel_name = eval('f' + repr(global_config['global']['slack_channel_template']))
+channel_name = course['sessions'][0]['slack_channel']
 
 if not slack.is_member(channel_name):
     slack.join_channel(channel_name)
 
 message = f"Username spreadsheet: {sheet_url}"
+start = to_iso8061(event['start']['local'])
 
 # post now
 slack.post_to_channel(channel_name, message)
