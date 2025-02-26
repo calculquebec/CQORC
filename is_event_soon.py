3ce11f2f35babaca3e01d8ef956688adb1c8f812
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-02-26 09:17:59 -0500

    add option to pass parameters through config file

--- is_event_soon.py
+++ is_event_soon.py
@@ -3,12 +3,15 @@ import os, argparse, itertools
 from datetime import datetime, timedelta, date
 import pytz
 import requests
+import configparser
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--calendar_id", help="Calendar ID to use")
 parser.add_argument("--course", help="Search for event with corresponding course code in the title")
 parser.add_argument("--delay", help="How many minutes in advance to check")
 parser.add_argument("--apikey", help="Google API key to use to query calendar")
+parser.add_argument("--config", help="Config file that contains secrets")
 args = parser.parse_args()
 
 tzinfo = pytz.timezone("America/Montreal")
@@ -21,9 +24,14 @@ if start_time.tzinfo is None:
 start_time = start_time.isoformat()
 end_time = end_time.isoformat()
 
+if args.config:
+    config = configparser.ConfigParser()
+    config.read(args.config)
+    calendar_id = args.calendar_id or config['DEFAULT']['calendar_id']
+    apikey = args.apikey or config['DEFAULT']['apikey']
 
 params = {
-    'key': args.apikey,
+    'key': apikey,
     'timeMin': start_time,
     'timeMax': end_time,
     'maxResults': '10',
@@ -33,7 +41,7 @@ params = {
 }
 
 response = requests.get(
-    f'https://www.googleapis.com/calendar/v3/calendars/{args.calendar_id}/events',
+    f'https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events',
     params=params,
 )
 
