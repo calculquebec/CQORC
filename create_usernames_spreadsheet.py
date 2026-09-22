3b395d355e7b3d6b411730b1b06e44749bc649fd
Helene Gingras <helene.gingras@calculquebec.ca>
2026-09-22 10:44:06 -0400

    Séparé le issue de la langue dans une autre PR.

--- create_usernames_spreadsheet.py
+++ create_usernames_spreadsheet.py
@@ -83,12 +83,7 @@ if not course:
 attendees = eb.get_event_attendees_registered(eventbrite_id, fields = ['email', 'name'])
 
 date = to_iso8061(event["start"]["local"]).date()
-title = get_title(course['sessions'][0])
-locale = str(course['sessions'][0]['language']).strip().lower()
-if locale.startswith('fr'):
-    locale = 'fr'
-elif locale.startswith('en'):
-    locale = 'en'
+locale = course['sessions'][0]['language']
 
 if args.course_code:
     course_code = args.course_code
